3935f4f5f4847f2081be6c2ae517eedfbb31d7df
florob <florian.robert@skillogs.com>
2019-01-19 16:44:10 +0100

    verif if value exist in token

--- src/components/AuthGuard/AuthGuard.jsx
+++ src/components/AuthGuard/AuthGuard.jsx
@@ -2,37 +2,36 @@ import React from 'react';
 import AuthService from '../../service/AuthService';
 
 export default function withAuth(Component) {
-
   return class AuthGuard extends React.Component {
-    constructor() {
-      super();
-      this.state = { user: null };
-    }
+      state = { user: null };
 
-    componentWillMount() {
-      const token = AuthService.getToken();
-      if (!token) {
+      componentWillMount() {
+        const token = AuthService.getToken();
+        if (!token) {
         // @todo addSnackBar
-        this.props.history.push('/404');
-      } else {
-        try {
-          const userInfo = AuthService.getUserInfo(token);
-          this.setState({ user: userInfo });
-        } catch (err) {
-          // @todo addSnackBar
           this.props.history.push('/404');
+        } else {
+          try {
+            const userInfo = AuthService.getUserInfo(token);
+            if (userInfo.email && userInfo.unique_name && userInfo.role) {
+              this.setState({ user: userInfo });
+            } else {
+              this.props.history.push('/404');
+            }
+          } catch (err) {
+          // @todo addSnackBar
+            this.props.history.push('/404');
+          }
         }
       }
-    }
 
-    render() {
-      if (this.state.user) {
-        return (
-          <Component history={this.props.history} user={this.state.user} />
-        );
-      } else {
+      render() {
+        if (this.state.user) {
+          return (
+            <Component history={this.props.history} user={this.state.user} />
+          );
+        }
         return null;
       }
-    }
   };
 }
