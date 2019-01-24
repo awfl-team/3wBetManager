a0bc4b908b15494208ce6d41b474d6b65c5e04b9
florob <florian.robert@skillogs.com>
2019-01-24 09:16:36 +0100

    fix bug component bestBetters

--- src/components/BestBetters/BestBettersCurrentUserTable.jsx
+++ src/components/BestBetters/BestBettersCurrentUserTable.jsx
@@ -1,35 +1,34 @@
 import React from 'react';
-import {Icon, Label, Table} from 'semantic-ui-react';
-import {Link} from 'react-router-dom';
+import { Icon, Label, Table } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 class BestBettersCurrentUserTable extends React.Component {
-
   render() {
     return (
       <div id="bestBettersCurrentUserTable">
         <Table celled structured compact inverted>
           <Table.Header>
             <Table.Row textAlign="center">
-              <Table.HeaderCell rowSpan='2'>#</Table.HeaderCell>
-              <Table.HeaderCell rowSpan='2'>Username</Table.HeaderCell>
-              <Table.HeaderCell rowSpan='2'>Score</Table.HeaderCell>
-              <Table.HeaderCell rowSpan='2'>Nb bets</Table.HeaderCell>
-              <Table.HeaderCell colSpan='3'>Nb bets per type</Table.HeaderCell>
-              <Table.HeaderCell rowSpan='2'>Actions</Table.HeaderCell>
+              <Table.HeaderCell rowSpan="2">#</Table.HeaderCell>
+              <Table.HeaderCell rowSpan="2">Username</Table.HeaderCell>
+              <Table.HeaderCell rowSpan="2">Score</Table.HeaderCell>
+              <Table.HeaderCell rowSpan="2">Nb bets</Table.HeaderCell>
+              <Table.HeaderCell colSpan="3">Nb bets per type</Table.HeaderCell>
+              <Table.HeaderCell rowSpan="2">Actions</Table.HeaderCell>
             </Table.Row>
             <Table.Row textAlign="center">
               <Table.HeaderCell>
-                <Label color='red'>
+                <Label color="red">
                   Wrong
                 </Label>
               </Table.HeaderCell>
               <Table.HeaderCell>
-                <Label color='orange'>
+                <Label color="orange">
                   Ok
                 </Label>
               </Table.HeaderCell>
               <Table.HeaderCell>
-                <Label color='green'>
+                <Label color="green">
                   Perfect
                 </Label>
               </Table.HeaderCell>
