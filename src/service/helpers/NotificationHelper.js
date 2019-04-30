0ff4f0b87491b2a0e317812cb8fd2b570a5435bc
lbourgeois <lucasbourgeois60@hotmail.fr>
2019-04-30 16:58:28 +0200

    Add icon on notification

--- src/service/helpers/NotificationHelper.js
+++ src/service/helpers/NotificationHelper.js
@@ -1,6 +1,6 @@
 export default class NotificationHelper {
   static createNotif(message) {
     // eslint-disable-next-line no-new
-    new Notification('Item used on you', { body: message });
+    new Notification('3wBetManager', { body: message, icon: 'assets/icons/icon-512x512.png' });
   }
 }
