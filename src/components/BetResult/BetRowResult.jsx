c83e97834befd33d9053fef2c0e933663354ecdc
lbourgeois <lucasbourgeois60@hotmail.fr>
2019-05-20 16:50:23 +0200

    Add betResult skeleton layout

--- src/components/BetResult/BetRowResult.jsx
+++ src/components/BetResult/BetRowResult.jsx
@@ -5,95 +5,105 @@ import {
 import moment from 'moment';
 import BetHttpService from '../../httpServices/BetHttpService';
 import Bet from '../../model/Bet';
+import ResultsBetsSkeleton from '../SkeletonLoaders/ResultsBetsSkeleton';
 
 class BetRowResult extends React.Component {
   state = {
     bets: [],
+    isLoading: true,
   };
 
   componentDidMount() {
     BetHttpService.getFinishBet(this.props.competitionId).then((response) => {
-      this.setState({ bets: response.data });
+      this.setState({
+        bets: response.data,
+        isLoading: false,
+      });
     });
   }
 
   render() {
-    const { bets } = this.state;
+    const { bets, isLoading } = this.state;
     return (
       <div id="betRows">
-        <Container fluid>
-          {bets.map(bet => (
-            <div
-              key={bet.Id}
-              className={`betRow ${bet.Multiply === 10 ? 'buffed-x10' : ''
-              || bet.Multiply === 5 ? 'buffed-x5' : ''
-              || bet.Multiply === 2 ? 'buffed-x2' : ''}`}
-            >
-              <div className="betRow-info">
-                <div className="container-hometeam">
-                  <div className="team-image">
-                    <Image
-                      src={bet.Match.HomeTeam.CrestUrl ? bet.Match.HomeTeam.CrestUrl : '/assets/images/hometeam-placeholder.png'}
-                      onError={(e) => { e.target.onerror = null; e.target.src = '/assets/images/hometeam-placeholder.png'; }}
-                      className={bet.Match.Score.Winner === 'HOME_TEAM' ? 'win' : 'loose'}
-                    />
+        { isLoading ? (
+          <ResultsBetsSkeleton />
+        ) : (
+          <Container fluid>
+            {bets.map(bet => (
+              <div
+                key={bet.Id}
+                className={`betRow ${bet.Multiply === 10 ? 'buffed-x10' : ''
+                || bet.Multiply === 5 ? 'buffed-x5' : ''
+                || bet.Multiply === 2 ? 'buffed-x2' : ''}`}
+              >
+                <div className="betRow-info">
+                  <div className="container-hometeam">
+                    <div className="team-image">
+                      <Image
+                        src={bet.Match.HomeTeam.CrestUrl ? bet.Match.HomeTeam.CrestUrl : '/assets/images/hometeam-placeholder.png'}
+                        onError={(e) => { e.target.onerror = null; e.target.src = '/assets/images/hometeam-placeholder.png'; }}
+                        className={bet.Match.Score.Winner === 'HOME_TEAM' ? 'win' : 'loose'}
+                      />
+                    </div>
+                    <div className="team-info">
+                      <div className="team-name">
+                        {bet.Match.HomeTeam.Name}
+                      </div>
+                    </div>
                   </div>
-                  <div className="team-info">
-                    <div className="team-name">
-                      {bet.Match.HomeTeam.Name}
+                  <div className="container-versus">
+                    <div className="match-info">{moment(bet.Match.UtcDate).format('MM-DD-YYYY')}</div>
+                    <div className="container-versus-details">
+                      <div className="home-score ">{bet.Match.Score.FullTime.HomeTeam}</div>
+                      <div className="versus-text"> -</div>
+                      <div className="away-score loose">{bet.Match.Score.FullTime.AwayTeam}</div>
+                    </div>
+                    <div className="container-versus-details-results">
+                      <div className="home-score">{bet.HomeTeamScore}</div>
+                      <div className="versus-text"> -</div>
+                      <div className="away-score">{bet.AwayTeamScore}</div>
+                    </div>
+                  </div>
+                  <div className="container-awayteam">
+                    <div className="team-image">
+                      <Image
+                        src={bet.Match.AwayTeam.CrestUrl ? bet.Match.AwayTeam.CrestUrl : '/assets/images/awayteam-placeholder.png'}
+                        onError={(e) => { e.target.onerror = null; e.target.src = '/assets/images/awayteam-placeholder.png'; }}
+                        className={bet.Match.Score.Winner === 'Away_TEAM' ? 'win' : 'loose'}
+                      />
+                    </div>
+                    <div className="team-info">
+                      <div className="team-name">
+                        {bet.Match.AwayTeam.Name}
+                      </div>
                     </div>
                   </div>
                 </div>
-                <div className="container-versus">
-                  <div className="match-info">{moment(bet.Match.UtcDate).format('MM-DD-YYYY')}</div>
+                <div className="betRow-results">
+                  <Message.Header>
+                    {bet.PointsWon}
+                    {' '}
+                    <Icon color="yellow" name="copyright" />
+                  </Message.Header>
                   <div className="container-versus-details">
-                    <div className="home-score ">{bet.Match.Score.FullTime.HomeTeam}</div>
-                    <div className="versus-text"> -</div>
-                    <div className="away-score loose">{bet.Match.Score.FullTime.AwayTeam}</div>
-                  </div>
-                  <div className="container-versus-details-results">
-                    <div className="home-score">{bet.HomeTeamScore}</div>
-                    <div className="versus-text"> -</div>
-                    <div className="away-score">{bet.AwayTeamScore}</div>
-                  </div>
-                </div>
-                <div className="container-awayteam">
-                  <div className="team-image">
-                    <Image
-                      src={bet.Match.AwayTeam.CrestUrl ? bet.Match.AwayTeam.CrestUrl : '/assets/images/awayteam-placeholder.png'}
-                      onError={(e) => { e.target.onerror = null; e.target.src = '/assets/images/awayteam-placeholder.png'; }}
-                      className={bet.Match.Score.Winner === 'Away_TEAM' ? 'win' : 'loose'}
-                    />
-                  </div>
-                  <div className="team-info">
-                    <div className="team-name">
-                      {bet.Match.AwayTeam.Name}
+                    <div className="bet-date">{moment(bet.Date).format('MM-DD-YYYY')}</div>
+                    <div className="container-versus-details-results-bet">
+                      <Label className={
+                        bet.Status === Bet.STATUS_PERFECT ? 'greenLabel' : ''
+                        || bet.Status === Bet.STATUS_WRONG ? 'redLabel' : ''
+                        || bet.Status === Bet.STATUS_OK ? 'orangeLabel' : ''}
+                      >
+                        {bet.Status}
+                      </Label>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="betRow-results">
-                <Message.Header>
-                  {bet.PointsWon}
-                  {' '}
-                  <Icon color="yellow" name="copyright" />
-                </Message.Header>
-                <div className="container-versus-details">
-                  <div className="bet-date">{moment(bet.Date).format('MM-DD-YYYY')}</div>
-                  <div className="container-versus-details-results-bet">
-                    <Label className={
-                      bet.Status === Bet.STATUS_PERFECT ? 'greenLabel' : ''
-                      || bet.Status === Bet.STATUS_WRONG ? 'redLabel' : ''
-                      || bet.Status === Bet.STATUS_OK ? 'orangeLabel' : ''}
-                    >
-                      {bet.Status}
-                    </Label>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </Container>
+            ))}
+          </Container>
+        )}
+
       </div>
     );
   }
--- src/components/SkeletonLoaders/ResultsBetsSkeleton.jsx
+++ src/components/SkeletonLoaders/ResultsBetsSkeleton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ContentLoader from 'react-content-loader';
 
 
-class ResultsAndSubmitBetsSkeleton extends React.Component {
+class ResultsBetsSkeleton extends React.Component {
   render() {
     return (
       <ContentLoader
@@ -14,29 +14,25 @@ class ResultsAndSubmitBetsSkeleton extends React.Component {
       >
         <circle cx="405" cy="40" r="40" />
         <circle cx="1275" cy="40" r="40" />
-        <rect x="830" y="70" rx="0" ry="0" width="40" height="20" />
         <rect x="285" y="130" rx="0" ry="0" width="250" height="20" />
         <rect x="1163" y="130" rx="0" ry="0" width="250" height="20" />
         <rect x="330" y="100" rx="0" ry="0" width="156" height="20" />
         <rect x="1200" y="100" rx="0" ry="0" width="156" height="20" />
-        <rect x="700" y="50" rx="0" ry="0" width="70" height="70" />
+        <rect x="700" y="30" rx="0" ry="0" width="300" height="60" />
+        <rect x="700" y="110" rx="0" ry="0" width="300" height="60" />
         <rect x="700" y="220" rx="0" ry="0" width="300" height="4" />
-        <rect x="930" y="50" rx="0" ry="0" width="70" height="70" />
-        <rect x="815" y="140" rx="0" ry="0" width="70" height="20" />
         <circle cx="405" cy="280" r="40" />
         <circle cx="1275" cy="280" r="40" />
-        <rect x="830" y="290" rx="0" ry="0" width="40" height="20" />
         <rect x="285" y="370" rx="0" ry="0" width="250" height="20" />
         <rect x="1163" y="370" rx="0" ry="0" width="250" height="20" />
         <rect x="330" y="340" rx="0" ry="0" width="156" height="20" />
         <rect x="1200" y="340" rx="0" ry="0" width="156" height="20" />
-        <rect x="700" y="270" rx="0" ry="0" width="70" height="70" />
+        <rect x="700" y="280" rx="0" ry="0" width="300" height="60" />
+        <rect x="700" y="360" rx="0" ry="0" width="300" height="60" />
         <rect x="700" y="450" rx="0" ry="0" width="300" height="4" />
-        <rect x="930" y="270" rx="0" ry="0" width="70" height="70" />
-        <rect x="815" y="370" rx="0" ry="0" width="70" height="20" />
       </ContentLoader>
     );
   }
 }
 
-export default ResultsAndSubmitBetsSkeleton;
+export default ResultsBetsSkeleton;
